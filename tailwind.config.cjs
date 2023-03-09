4553cf963b8f925cf4b583079ffbf492d7eaa311
Erick Ramirez Trujillo <erickramirez.dnbk@gmail.com>
2023-03-09 13:57:25 -0500

    tailwind css installed

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ["./src/**/*.js"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
