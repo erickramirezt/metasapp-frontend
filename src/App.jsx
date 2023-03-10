5045e7914ca30fd1930bb0561b7ffe4c1f8b07f8
Erick Ramirez Trujillo <erickramirez.dnbk@gmail.com>
2023-03-10 10:44:11 -0500

    HTML meta created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Encabezado from "./componentes/compartidos/Encabezado";
 import Pie from "./componentes/compartidos/Pie";
 import Principal from "./componentes/compartidos/Principal";
+import Meta from "./componentes/lista/Meta";
 
 function App() {
   return (
     <div className="App">
       <Encabezado></Encabezado>
-      <Principal></Principal>
+      <Principal>
+        <Meta></Meta>
+      </Principal>
       <Pie></Pie>
     </div>
   );
---
+++ src/componentes/lista/Meta.jsx
@@ -0,0 +1,20 @@
+function Meta() {
+    return ( 
+        <div>
+            <div>
+                <div>icono</div>
+                <p>1<sub>/semana</sub></p>
+                <p>detalles</p>
+            </div>
+            <div>
+                <div>4 de 5</div>
+                <div>
+                    <div></div>
+                </div>
+                <button>Completado</button>
+            </div>
+        </div> 
+    );
+}
+
+export default Meta;
